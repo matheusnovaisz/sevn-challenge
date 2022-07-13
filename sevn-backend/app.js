70638d4cf597b156224ab613237fd95864fa9fab
Matheus Novais <matheusnovaisz@gmail.com>
2022-07-13 18:34:39 -0300

    ✨ add static serving images

--- sevn-backend/app.js
+++ sevn-backend/app.js
@@ -3,6 +3,7 @@ import { router } from "./routes.js";
 const app = express()
 const PORT = 5000;
 
+app.use(express.static('public'));
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
